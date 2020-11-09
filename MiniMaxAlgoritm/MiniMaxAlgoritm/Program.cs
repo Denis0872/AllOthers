eb82a4c0f2b43e67c5006da641e450e824537dc0
denis0872 <denispetergof@gmail.com>
2020-11-10 00:19:11 +0300

    1

--- MiniMaxAlgoritm/MiniMaxAlgoritm/Program.cs
+++ MiniMaxAlgoritm/MiniMaxAlgoritm/Program.cs
@@ -13,69 +13,66 @@ namespace MiniMaxAlgoritm
     class Program
     {
 
-        static int[] MinimaxArray = new int[] {0, -11, 2, 3, 4, 11};
+        static int[] MinimaxArray = new int[6];
+        //static int[] MinimaxArray = new int[] { -3, 11, 3, 5, 7, -12 };
         static int LndMax;
         static int LndMin;
-
         private static int[] MetodEnter()
         {
-           int num = 0;
-            do
-            {
-                int x;
-                Console.WriteLine("Введите элемент массива");
-                if (int.TryParse(Console.ReadLine(), out x))
-                    Console.WriteLine("Число введено корректно. Введите следующее");
-                else
-                    Console.WriteLine("Вы ввели некорректное число");
-                MinimaxArray[num] = x;
-                num++;
-            }
-             while (num < 6);
-           
-            return MinimaxArray;
-        }
-        #region Через For      
-        /*     {
-                 for (int i = 0; i < MinimaxArray.Length; i++)
-                 {
-                    int x;
-                     Console.WriteLine("Введите элемент массива");
-                     if (int.TryParse(Console.ReadLine(), out x))   
-                         Console.WriteLine("Число введено корректно. Введите следующее");
-                     else
-                         Console.WriteLine("Вы ввели некорректное число");
-                   MinimaxArray[i]=x;
-                 }
-
-             }*/
-        #endregion
-        static void MetodMM(int[] MiniMaxarray)
+            try
             {
                 for (int i = 0; i < MinimaxArray.Length; i++)
                 {
-                    if (MinimaxArray[i] > LndMax)
+                    int x;
+
+                    Console.WriteLine("Введите элемент массива");
+
+                    int p = Convert.ToInt32(Console.ReadLine());
+                    x = p;
+                    MinimaxArray[i] = x;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return MinimaxArray;     
+        }
+            static void MetodMM(int[] MiniMaxarray)
+            {
+
+               LndMax = MinimaxArray[0] ; //присваиваем значение первого элемента массива переменной LndMax
+               LndMin = MinimaxArray[0] ; //присваиваем значение первого элемента массива переменной LndMin
+                for (int i = 1; i < MinimaxArray.Length; i++) //цикл начинается со второго элемена
+                {
+
+                    if (MinimaxArray[i] > LndMax) //второй элемент сравнивается с первой переменной
                     {
-                        LndMax = MinimaxArray[i];
+                        LndMax = MinimaxArray[i]; //в переменную записывается новое число
                     }
-                    else if (MinimaxArray[i] < LndMin)
+                    else if (MinimaxArray[i] < LndMin)// ...если не удовлетворяет условию if то со второй
                     {
-                        LndMin = MinimaxArray[i];
+                        LndMin = MinimaxArray[i]; //в переменную записывается новое число
                     }
 
                 }
-                Console.WriteLine("LndMax=" + " " + LndMax);
-                Console.WriteLine("LndMin=" + " " + LndMin);
+                Console.WriteLine("LndMax=" + " " + LndMax); //вывод максимального значения
+                Console.WriteLine("LndMin=" + " " + LndMin);//  вывод минимального значения
             }
+
 
             static void Main(string[] args)
             {
+                ClassLibrary1.Engine eng = new ClassLibrary1.Engine();
                 MetodEnter();
                 MetodMM(MinimaxArray);
+                eng.Run();
                 Console.ReadKey();
             }
-        }
     }
+}
+
 
 
 
