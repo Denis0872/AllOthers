26a1598e6084c257a9b4e3c0ba17a9493196f98b
denis0872 <denispetergof@gmail.com>
2020-11-11 17:39:05 +0300

    1

---
+++ greetings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+
+            Console.WriteLine("введите своё имя");
+            myName = Console.ReadLine();
+            Console.WriteLine("Добрый день {0}", myName);
+            Console.ReadKey();
+        }
+    }
+}
